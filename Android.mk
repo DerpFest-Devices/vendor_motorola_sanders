71be4547231ec40190ec5c5385058c33e552b4fb
trpsl <trpsl755@gmail.com>
2020-03-13 01:02:42 +0100

    sanders: Add Moto Dolby blobs
    * from lake 'QPW30.61-18'
    
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- Android.mk
+++ Android.mk
@@ -287,4 +287,28 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := daxService
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/product/priv-app/daxService/daxService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRODUCT_SPECIFIC := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := MotoDolbyV3
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/app/MotoDolbyV3/MotoDolbyV3.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
 endif
